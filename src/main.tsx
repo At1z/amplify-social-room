a01461fe8c474102bb3298667ee62e3448843fc7
At1z <Adrianlistkiewiczv2@gmail.com>
2024-12-07 16:10:18 +0100

    auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Amplify } from 'aws-amplify';
-import '@aws-amplify/ui-react/styles.css'
-import outputs from '../amplify_outputs.json'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import outputs from "../amplify_outputs.json";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Authenticator>
       <App />
-  </React.StrictMode>,
-)
+    </Authenticator>
+  </React.StrictMode>
+);
